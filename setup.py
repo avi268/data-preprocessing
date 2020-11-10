ca18b34506aaf8e00c7879ddc36e8ae13e030ed2
Avinash Gunda <avinash.gunda1994@gmail.com>
2020-11-10 22:56:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-	name = 'Data Preprocess', #this should be unique
+	name = 'DataPreprocess', #this should be unique
 	include_package_data=True,
 	version = '0.1.0',
 	author = 'Avinash Gunda',
